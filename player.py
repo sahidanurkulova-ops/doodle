9b631b1323f3781856a3869585f0ab9954726180
Daniiar <sahidanurkulova@gmail.com>
2026-05-01 19:27:01 +0100

    Making bullet save.

--- player.py
+++ player.py
@@ -82,6 +82,7 @@ class Player:
         self.bullet_y = 0
         self.bullet_width = 15
         self.bullet_height = 12
+        self.bullet_visibility = False
         self.bullet = pygame.image.load("assets/images/doodle_jump_bullet.png")
         self.bullet_hitbox = pygame.Rect(self.bullet_x, self.bullet_y, self.bullet_width, self.bullet_height)
 
@@ -108,18 +109,21 @@ class Player:
             self.hitbox.y = self.rect.y
             self.hitbox.width = self.rect.width
             self.hitbox.height = self.rect.height
-            self.bullet_x = self.rect.x
+            self.bullet_visibility = False
+            self.bullet_x = self.rect.x + 24
             self.bullet_y = self.rect.y
         if self.shoot:
             self.hitbox.x = self.rect.x + 15
             self.hitbox.y = self.rect.y
             self.hitbox.width = self.width_shooting - 30
             self.hitbox.height = self.rect.height
-            self.bullet_y += 1
+            self.bullet_visibility = True
+            self.bullet_y -= 10
 
     def draw(self, screen):
         screen.blit(self.image_player, (self.rect.x, self.rect.y))
-        screen.blit(self.bullet, (self.bullet_x, self.bullet_y))
+        if self.bullet_visibility:
+            screen.blit(self.bullet, (self.bullet_x, self.bullet_y))
 
         if self.shoot:
             if self.state != "ghost" or self.state != "doodlestein":
