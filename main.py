1f5f9d5e4e752961f324b2d72518438454160982
Dominionss <qshieldt@gmail.com>
2026-09-18 21:34:29 +0300

    Breaking platforms fix

--- main.py
+++ main.py
@@ -38,17 +38,17 @@ monster = Monster(player.rect.y)
 volume_controller = VolumeControl(100, 700, 300)
 
 def create_platforms():
-    result = [Platform(WIDTH, "green", 225, 670)]
+    result = [Platform("green", 225, 670)]
 
     current_y = 720
     for _ in range(8):
         y = random.randint(1, 100)
         x = random.randint(0, WIDTH - 100)
         if y >= 1 and y <= 70:
-            result.append(Platform(WIDTH, "green", x, current_y))
+            result.append(Platform("green", x, current_y))
             print("green")
         else:
-            result.append(Platform(WIDTH, "blue", x, current_y))
+            result.append(Platform("blue", x, current_y))
             print("blue")
         current_y -= random.randint(70, 110)
 
@@ -80,11 +80,11 @@ def generate_new_platform(platforms):
     y = highest_y - random.randint(70, 110)
     z = random.randint(1, 100)
     if z >= 1 and z <= 70:
-        platforms.append(Platform(WIDTH, "green", x, y))
+        platforms.append(Platform("green", x, y))
     elif z >= 71 and z <= 90:
-        platforms.append(Platform(WIDTH, "blue", x, y))
+        platforms.append(Platform("blue", x, y))
     else:
-        platforms.append(Platform(WIDTH, "breaking", x, y))
+        platforms.append(Platform("breaking", x, y))
 
 
 def reset_game():
